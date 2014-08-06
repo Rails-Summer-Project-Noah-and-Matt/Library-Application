43f0edcd268ee23ca32f64aec69d28d2087a3fd8
Noah Birnel <nbirnel@gmail.com>
2014-08-05 18:49:36 -0700

    test invalid attributes in book

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -27,9 +27,9 @@ RSpec.describe BooksController, :type => :controller do
                              is_active: true
   } }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) { { title: '',
+                               is_active: true
+  } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
