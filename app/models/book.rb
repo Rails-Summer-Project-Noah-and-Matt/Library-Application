09c3a1ee36939c602a04b0817ec673c272597ef8
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-08-11 19:37:45 -0700

    Added owner_id to Book, and updated Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ActiveRecord::Base
     minimum: 1, 
     maximum: 256,
     message: 'Titles must be between 1 and 256 characters long' }
+  belongs_to :owner, class_name: User
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806050630) do
+ActiveRecord::Schema.define(version: 20140812023534) do
 
   create_table "books", force: true do |t|
     t.string   "title"
     t.boolean  "is_active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "owner_id"
   end
 
   create_table "users", force: true do |t|
