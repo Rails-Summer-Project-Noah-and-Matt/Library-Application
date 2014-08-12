acd221a2f99ab30b4a4c7b4759e4812de5734e7a
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-08-11 19:44:43 -0700

    added subscriptions model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812023534) do
+ActiveRecord::Schema.define(version: 20140812024420) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140812023534) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "owner_id"
+  end
+
+  create_table "subscriptions", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
