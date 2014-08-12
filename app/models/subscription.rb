1226c05f583305949df87dbb6f765c7430f40009
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-08-11 20:09:57 -0700

    updated models and added validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,7 @@
 class Subscription < ActiveRecord::Base
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "Book"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+
 end
