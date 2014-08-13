dd54b2b72bff36dd5d94ca25a443e295323232c4
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-08-12 18:25:21 -0700

    removed tests from subscription model

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,23 +1,5 @@
 require 'rails_helper'
 
-
-describe Subscription, :model  do
-  before(:example) do 
-    @user = User.create!(email: "ugh@test.com", password: "password", password_confirmation: "password")
-    @book1 = Book.create!(title: "This is a test book", owner_id: 1)
-    @book2 = Book.create!(title: "This is another test book", owner_id: 2)
-  end
-
-  it "should let a user follow a book" do
-    assert @user.follow!(@book1)
-  end
-
-  it "should return a subscription object if the user is following a book" do
-    @user.follow!(@book1)
-    assert @user.following?(@book1)
-  end
-   
-  it "shouldn't return a subscription object if the user isn't following a book" do
-    assert !@user.following?(@book2)
-  end
+RSpec.describe Subscription, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
