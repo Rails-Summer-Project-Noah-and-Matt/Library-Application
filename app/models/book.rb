e96de06103142972c6288413aa165df592439640
Noah Birnel <nbirnel@gmail.com>
2014-08-12 19:35:41 -0700

    validate isbn lengths

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,19 @@ class Book < ActiveRecord::Base
   validates :title, presence: true, length: { 
     minimum: 1, 
     maximum: 256,
-    message: 'Titles must be between 1 and 256 characters long' }
+    message: 'Titles must be between 1 and 256 characters long'
+  }
   
+  validates :isbn10, length: { 
+    is: 10,
+    message: 'ISBN10 is a 10 digit format'
+  }
+  
+  validates :isbn13, length: { 
+    is: 13,
+    message: 'ISBN13 is a 13 digit format'
+  }
+
   belongs_to :owner, class_name: User
  
   has_many :subscriptions, foreign_key: "followed_id", dependent: :destroy 
