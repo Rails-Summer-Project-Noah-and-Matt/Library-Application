6c479f940a4badcf13bf57f186285444da88d778
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-08-12 19:39:58 -0700

    added review model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812024420) do
+ActiveRecord::Schema.define(version: 20140813023144) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20140812024420) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "owner_id"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.text     "text"
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "subscriptions", force: true do |t|
