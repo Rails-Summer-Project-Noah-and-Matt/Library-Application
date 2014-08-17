a2d767f2e6e2cbefa2cf5f16bd3a42d30d9ef6b9
Noah Birnel <nbirnel@gmail.com>
2014-08-16 22:33:49 -0700

    make author select work from book new
    permit author_id in books_controller
    change :author to :book in books_view authors collection
    use author.sortable_name in book index

--- db/schema.rb
+++ db/schema.rb
@@ -35,14 +35,6 @@ ActiveRecord::Schema.define(version: 20140817003949) do
   add_index "books", ["isbn10"], name: "index_books_on_isbn10"
   add_index "books", ["isbn13"], name: "index_books_on_isbn13"
 
-  create_table "reviews", force: true do |t|
-    t.text     "text"
-    t.integer  "user_id"
-    t.integer  "book_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "subscriptions", force: true do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
