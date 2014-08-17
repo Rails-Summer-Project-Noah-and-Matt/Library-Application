9a927abb0949523d22e3259adc847942f4af3566
Noah Birnel <nbirnel@gmail.com>
2014-08-16 23:18:59 -0700

    test for no destroy published authors

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Author, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @author = Author.create(given_name: "Joe", family_name: "Smith")
+    @book = Book.create(title: "Joe's Book", is_active: true, author_id: 1)
+  end
+
+  it "should refuse to destroy an author with books" do
+    expect { @author.destroy }.to_not change(Book, :count)
+  end
 end
