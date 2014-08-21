207cb5b4f6df806d3fa9962de4da891f64f62b84
Noah Birnel <nbirnel@gmail.com>
2014-08-21 13:16:18 -0700

    restore controllers to their rails g style
    cd app/controllers
    for i in *rb; do
        cp -v $i $i.tmp
        git checkout 647b81be419d8acd78f35a65de099e1cff054d67~1 $i
        vimdiff $i $i.tmp
    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,48 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def respond_to_create params
-    thing    = params[:thing]
-    redirect = params[:redirect] ? params[:redirect] : thing
-    name     = params[:name]     ? params[:name]     : thing.class.to_s
-
-    respond_to do |format|
-      if thing.save
-        format.html { redirect_to redirect, notice: "#{name} was successfully created." }
-        format.json { render :show, status: :created, location: redirect }
-      else
-        format.html { render :new }
-        format.json { render json: thing.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def respond_to_update params
-    thing    = params[:thing]
-    redirect = params[:redirect] ? params[:redirect] : thing
-    name     = params[:name]     ? params[:name]     : thing.class.to_s
-    thing_params = params[:thing_params] ? params[:thing_params] : eval(thing.class.to_s.downcase.sub(/$/, "_params"))
-
-    respond_to do |format|
-      if thing.update(thing_params)
-        format.html { redirect_to redirect, notice: "#{name} was successfully updated." }
-        format.json { render :show, status: :created, location: redirect }
-      else
-        format.html { render :edit }
-        format.json { render json: thing.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def respond_to_destroy params
-    thing    = params[:thing]
-    redirect = params[:redirect]
-    name     = params[:name]     ? params[:name]     : thing.class.to_s
-
-    respond_to do |format|
-      format.html { redirect_to redirect, notice: "#{name} was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 end
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,45 +1,46 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
-  # GET /authors
-  # GET /authors.json
   def index
     @authors = Author.all
   end
 
-  # GET /authors/1
-  # GET /authors/1.json
-  def show
-  end
-
-  # GET /authors/new
   def new
     @author = Author.new
   end
 
-  # GET /authors/1/edit
-  def edit
-  end
-
-  # POST /authors
-  # POST /authors.json
   def create
     @author = Author.new(author_params)
 
-    respond_to_create({thing: @author})
+    respond_to do |format|
+      if @author.save
+        format.html { redirect_to @author, notice: 'Author was successfully created.' }
+        format.json { render :show, status: :created, location: @author }
+      else
+        format.html { render :new }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  # PATCH/PUT /authors/1
-  # PATCH/PUT /authors/1.json
   def update
-    respond_to_update({thing: @author})
+    respond_to do |format|
+      if @author.update(author_params)
+        format.html { redirect_to @author, notice: 'Author was successfully updated.' }
+        format.json { render :show, status: :ok, location: @author }
+      else
+        format.html { render :edit }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  # DELETE /authors/1
-  # DELETE /authors/1.json
   def destroy
     @author.destroy
-    respond_to_destroy({thing: @author, redirect: authors_url})
+    respond_to do |format|
+      format.html { redirect_to authors_url, notice: 'Author was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
