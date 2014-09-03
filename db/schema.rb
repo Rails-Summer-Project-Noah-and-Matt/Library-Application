c87125e76b00ac70cefb35cfd08ed43855da3fd9
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-09-02 17:40:44 -0700

    changed pg to production db

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20140902183001) do
     t.datetime "updated_at"
   end
 
+  create_table "average_caches", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "avg",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "books", force: true do |t|
     t.string   "title"
     t.boolean  "is_active",    default: true
@@ -31,7 +40,7 @@ ActiveRecord::Schema.define(version: 20140902183001) do
     t.integer  "author_id"
     t.string   "cover"
     t.boolean  "approved",     default: false
-    t.float    "rating",       default: 0.0
+    t.float    "rating"
     t.integer  "true_user_id"
   end
 
@@ -48,13 +57,46 @@ ActiveRecord::Schema.define(version: 20140902183001) do
     t.datetime "updated_at"
   end
 
+  create_table "overall_averages", force: true do |t|
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "overall_avg",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rates", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "stars",         null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id"
+
+  create_table "rating_caches", force: true do |t|
+    t.integer  "cacheable_id"
+    t.string   "cacheable_type"
+    t.float    "avg",            null: false
+    t.integer  "qty",            null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+
   create_table "reviews", force: true do |t|
     t.text     "text"
     t.integer  "user_id"
     t.integer  "book_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "rating",       default: 0
+    t.integer  "rating"
     t.integer  "true_user_id"
   end
 
